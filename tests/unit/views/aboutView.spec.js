571ebce53c1a19b6d2429791441ca6fd7b842470
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-10-29 13:10:33 -0300

    test de existencia de AboutView

---
+++ tests/unit/views/aboutView.spec.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import AboutView from '@/views/AboutView.vue'
+
+
+
+describe('AboutView', () => { 
+    test('Probando la existencia de la vista AboutView ', async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes: [{
+                path: '/about',
+                name: 'AboutView',
+                component: AboutView
+            }],
+        })
+        router.push('/about')
+        await router.isReady()
+
+        const wrapper = mount(AboutView, {
+            global: {
+                plugins: [router]
+            }
+        })
+        expect(wrapper.findComponent(AboutView).exists()).toBe(true)
+    })  
+ })
