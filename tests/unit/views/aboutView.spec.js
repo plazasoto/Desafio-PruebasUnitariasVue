5406f84880bcaba8516dbbd228e38e7a1d011f19
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-10-29 17:30:43 -0300

    prueba snapshot de AboutView

--- tests/unit/views/aboutView.spec.js
+++ tests/unit/views/aboutView.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import AboutView from '@/views/AboutView.vue'
@@ -22,5 +22,10 @@ describe('Componente AboutView', () => {
             }
         })
         expect(wrapper.findComponent(AboutView).exists()).toBe(true)
-    })  
+    })
+
+    test('Validación de match con snapshot', () => {
+        const wrapper = shallowMount(AboutView)
+        expect(wrapper.html()).toMatchSnapshot()
+    })
  })
--- tests/unit/views/homeView.spec.js
+++ tests/unit/views/homeView.spec.js
@@ -2,8 +2,8 @@ import { shallowMount } from '@vue/test-utils'
 import HomeView from '@/views/HomeView.vue'
 
 describe('Componente HomeView', () => {
-  test('Validación de match con snapshot', () => {
-    const wrapper = shallowMount(HomeView)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+    test('Validación de match con snapshot', () => {
+        const wrapper = shallowMount(HomeView)
+        expect(wrapper.html()).toMatchSnapshot()
+    })
 })
